b5e9923039b0d03b1ab3436bcd9305351a771e89
Alexandre <costasalexandre511@gmail.com>
2022-10-16 16:04:48 -0300

    add playground

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,5 +1,10 @@
+import { Envelope, Lock } from "phosphor-react";
+
 import { Button } from "@tails-ui/button";
 import { Text } from "@tails-ui/text";
+import { Label } from "@tails-ui/label";
+import { TextInput } from "@tails-ui/text-input";
+import { Checkbox } from "@tails-ui/checkbox";
 
 import { Logo } from "./components/Logo";
 
@@ -7,18 +12,75 @@ import "./styles/global.css";
 
 export function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-white">
-      <header>
+    <div className="w-screen flex flex-col items-center justify-center text-white">
+      <header className="flex flex-col items-center">
         <Logo />
 
-        <Text.Heading size="lg">
+        <Text.Heading size="lg" className="mt-4">
           React Tails UI
         </Text.Heading>
 
-        <Text size="lg" className="text-gray-400">
+        <Text size="lg" className="text-gray-400 mt-1">
           Sign In and get started now!
         </Text>
       </header>
+
+      <form className="w-full max-w-[400px] flex flex-col gap-4 items-stretch mt-10">
+        <Label htmlFor="email" className="font-semibold flex flex-col gap-3">
+          E-mail address
+          <TextInput>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="alexandre@gmail.com"
+            />
+          </TextInput>
+        </Label>
+
+        <Label htmlFor="password" className="font-semibold flex flex-col gap-3">
+          Your password
+          <TextInput>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="***********"
+            />
+          </TextInput>
+        </Label>
+
+        <div className="flex items-center gap-2">
+          <Checkbox id="remember-me" name="remember-me">
+            <Checkbox.Icon />
+          </Checkbox>
+
+          <Label htmlFor="remember-me">
+            <Text size="sm" className="text-gray-200">
+              Remember me for 30 days
+            </Text>
+          </Label>
+        </div>
+
+        <Button type="submit" className="mt-4">Get Started</Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text size="sm" className="text-gray-400 underline hover:text-gray-200" asChild>
+          <a href="#">Forgot your password?</a>
+        </Text>
+        <Text size="sm" className="text-gray-400 underline hover:text-gray-200" asChild>
+          <a href="#">Still don’t have an account? Register now!</a>
+        </Text>
+      </footer>
     </div>
   );
 }
--- packages/components/Button/src/Button.tsx
+++ packages/components/Button/src/Button.tsx
@@ -1,10 +1,10 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes, HTMLAttributes } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 import { OverridableChildrenComponent } from "@tails-ui/typings";
 
 export type ButtonProps = OverridableChildrenComponent &
-  HTMLAttributes<HTMLButtonElement>;
+  ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button({ children, asChild, className, ...rest }: ButtonProps) {
   const Comp = asChild ? Slot : "button";
@@ -13,7 +13,7 @@ export function Button({ children, asChild, className, ...rest }: ButtonProps) {
     <Comp
       className={clsx(
         className,
-        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
+        "py-3 px-4 h-10 bg-cyan-500 rounded font-semibold text-black text-sm transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
       )}
       {...rest}
     >
