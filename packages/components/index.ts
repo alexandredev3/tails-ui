beab04c62c607e5ed674c73f3c769ad3aa5e53df
Alexandre <costasalexandre511@gmail.com>
2023-01-18 11:43:38 -0300

    add changesets setup

--- packages/components/index.ts
+++
@@ -1,7 +0,0 @@
-import '@tails-ui/styles/common.css';
-
-export * from './Button';
-export * from './Typography';
-export * from './Heading';
-export * from './TextInput';
-export * from './Checkbox';
