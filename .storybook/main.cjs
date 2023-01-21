af6fafddf24f6b595a2870ceb2d0b6575c934af0
Alexandre <costasalexandre511@gmail.com>
2023-01-21 13:50:10 -0300

    add storybook mdx docs preset

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@tails-ui/storybook-mdx-docs",
     {
       name: "@storybook/addon-postcss",
       options: {
